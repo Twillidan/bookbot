335e43278393346338783b6cb74d0925a07fa1fb
Twillidan <sunlg94@gmail.com>
2024-11-08 14:16:49 +0100

    Added args handeling so the user can opt for value or key ordering

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import argparse
+
 def main():
+    args = parser.parse_args()
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_count = count_words(text)
-    character_count = count_character(text)
+    character_count = count_character(text, args.order)
     print(text)
     print(f"--- Begining report for {book_path} ---")
     print(f"{word_count} Words found in this book")
@@ -22,15 +25,19 @@ def count_words(string):
     count = len(words)
     return count
 
-def count_character(string):
+def count_character(string, order=1):
     lowercase_string = string.lower()
     characters = {}
+    reverse_sort = order == 1
     for c in lowercase_string:
         if c.isalpha():
             characters[c] = characters.get(c, 0) + 1
     #sorted_characters = dict(sorted(characters.items()))
-    sorted_characters = dict(sorted(characters.items(), key=lambda item: item[1], reverse=True))
+    sorted_characters = dict(sorted(characters.items(), key=lambda item: item[order], reverse=reverse_sort))
     return (sorted_characters)
 
 if __name__ == "__main__":
+        # Setup the argument parser
+    parser = argparse.ArgumentParser(description='Sort character frequency.')
+    parser.add_argument('order', type=int, choices=[0, 1], help='0 for alphabetical, 1 for frequency')
     main()
