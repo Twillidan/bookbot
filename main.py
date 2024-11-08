a5de21644964f45add85e454caec8916a743e87c
Twillidan <sunlg94@gmail.com>
2024-11-08 11:54:04 +0100

    Updated the character counting function to list acording to value instead of key, and nicer printout

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ def main():
     word_count = count_words(text)
     character_count = count_character(text)
     print(text)
-    print(f"This book contains {word_count} number of words")
+    print(f"--- Begining report for {book_path} ---")
+    print(f"{word_count} Words found in this book")
     print(f"List of how often each character appears")
-    print(character_count)
+    for c in character_count:
+        print(f"The letter {c} appears {character_count[c]} times")
+    #print(character_count)
+    print("--- End report ---")
 
 
 def get_book_text(path):
@@ -22,11 +26,11 @@ def count_character(string):
     lowercase_string = string.lower()
     characters = {}
     for c in lowercase_string:
-        if c in characters:
-            characters[c] += 1
-        else:
-            characters[c] = 1
-    return (characters)
+        if c.isalpha():
+            characters[c] = characters.get(c, 0) + 1
+    #sorted_characters = dict(sorted(characters.items()))
+    sorted_characters = dict(sorted(characters.items(), key=lambda item: item[1], reverse=True))
+    return (sorted_characters)
 
 if __name__ == "__main__":
     main()
